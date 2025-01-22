e309f1bafb2f1725b1afdddc233dedacbb03607f
um4ss <sliziard@icloud.com>
2025-01-22 15:31:35 +0100

    add format attribute to ft_printf(s)

--- src/ft_printf.h
+++ src/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 17:50:16 by sliziard          #+#    #+#             */
-/*   Updated: 2025/01/22 13:40:12 by sliziard         ###   ########.fr       */
+/*   Updated: 2025/01/22 15:30:54 by sliziard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,14 @@
 # endif
 
 
-int			ft_printf(const char *format, ...);
-int			ft_fprintf(int fd, const char *format, ...);
-int			ft_srprintf(char **str, size_t n, const char *format, ...);
-int			ft_snprintf(char *str, size_t n, const char *format, ...);
+int			ft_printf(const char *format, ...) \
+			__attribute__((format(printf, 1, 2)));
+int			ft_fprintf(int fd, const char *format, ...) \
+			__attribute__((format(printf, 2, 3)));
+int			ft_srprintf(char **str, size_t n, const char *format, ...) \
+			__attribute__((format(printf, 3, 4)));
+int			ft_snprintf(char *str, size_t n, const char *format, ...) \
+			__attribute__((format(printf, 3, 4)));
 
 int			ft_vsrprintf(char **str, size_t n, const char *format, va_list ap);
 int			ft_vfprintf(int fd, const char *format, va_list ap);
